f8740f8acda5a94d9c9ce8fe546aa301c2f61c6f
Sdurant11 <samdurant.teacher@gmail.com>
2019-08-17 18:55:41 +0000

    finished average grade

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
-function Header() {
+function GetAverage(props) {
+  var gradeNumArray = props.grades.map(studentInfoObj =>
+    studentInfoObj.grade
+  );
+
+  var gradeTotal = 0;
+  for (var i = 0; i < gradeNumArray.length; i++) {
+    gradeTotal += gradeNumArray[i];
+  }
+  var gradeAverage = (gradeTotal / gradeNumArray.length).toFixed(2);
+
+  return <div className = 'gradeAverageNumber'>Average Grade
+    <span className="badge badge-secondary">{gradeAverage}</span>
+  </div>;
+}
+
+function Header(props) {
   return (
-    <header>Student Grade Table</header>
+    <header className = 'title'>Student Grade Table
+      <GetAverage grades = {props.grades} />
+    </header>
   );
 }
 
